8bff98a0b360aef359239ae3b415063f092d4ec0
Daniel Ontiveros <cardanont@gmail.com>
2019-11-11 00:53:23 -0600

    Roman Parse Test 1st part

---
+++ CSharpTDD/RomanNumeral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTDD
+{
+    public class RomanNumeral
+    {
+        public static Dictionary<char, int> map =
+            new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50 },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 }
+        };
+
+        public static int Parse(string roman)
+        {
+            return map[roman[0]];
+        }
+    }
+}
---
+++ TDD-CSharp-Lib.Test/RomanNumeralTests.cs
@@ -0,0 +1,24 @@
+﻿using CSharpTDD;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_CSharp_Lib.Test
+{
+    [TestFixture]
+    class RomanNumeralTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(50, "L")]
+        [TestCase(100, "C")]
+        [TestCase(500, "D")]
+        [TestCase(1000, "M")]
+        public void Parse(int expected, string roman)
+        {
+            Assert.AreEqual(expected, RomanNumeral.Parse(roman));
+        }
+    }
+}
