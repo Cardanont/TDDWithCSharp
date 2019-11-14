b0b98739021b69c9b3d74d7c509cc8ddf60b8646
Daniel Ontiveros <cardanont@gmail.com>
2019-11-13 21:55:01 -0600

    Update Spin TDD

---
+++ CSharpTDD/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CSharpTDD
+{
+    public class Client
+    {
+        private readonly UpdateableSpin spin;
+        //private readonly Processor processor;
+
+        public Client()
+        {
+
+        }
+
+        public void Do()
+        {
+
+        }
+    }
+
+    public class UpdateableSpin
+    {
+        private readonly object lockObj = new object();
+        private bool shouldWait = true;
+
+        public bool Wait(TimeSpan timeout)
+        {
+            Thread.Sleep(timeout);
+            if (!shouldWait)
+                return true;
+            return false;
+        }
+
+        public void Set()
+        {
+            lock (lockObj)
+            {
+                shouldWait = false;
+            }
+        }
+    }
+}
