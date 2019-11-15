1dbb5a1a4f1ace875b51396fd727534990502840
Daniel Ontiveros <cardanont@gmail.com>
2019-11-14 22:41:22 -0600

    TicTacToe class and tests implementaiton 001

---
+++ CSharpTDD/TicTacToe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTDD
+{
+    public class TicTacToe
+    {
+
+        public int MovesCounter { get; private set; }
+
+        public void MakeMove(int index)
+        {
+            MovesCounter++;
+        }
+    }
+}
---
+++ TDD-CSharp-Lib.Test/TicTacToeTests.cs
@@ -0,0 +1,28 @@
+﻿using CSharpTDD;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_CSharp_Lib.Test
+{
+    [TestFixture]
+    class TicTacToeTests
+    {
+        [Test]
+        public void CreateGame_With_ZeroMoves()
+        {
+            TicTacToe game = new TicTacToe();
+            Assert.AreEqual(0, game.MovesCounter);
+        }
+
+        [Test]
+        public void MakeMove_CounterShift()
+        {
+            TicTacToe game = new TicTacToe();
+            game.MakeMove(1);
+
+            Assert.AreEqual(1, game.MovesCounter);
+        }
+    }
+}
